484435ef202f9a4922b2ef7485d7d65b000d0ab7
Jonathan Corley <>
2021-04-27 19:12:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+/** 
+ * Given a startsWith character, which will be a single character, return a 
+ * Function that tests whether a given String starts with the startsWith 
+ * character.
+ */
+function createStartsWithFilter(string,startsWith) {
+    // YOUR CODE BELOW HERE //
+
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+
+/** 
+ * Given a endsWith character, which will be a single character, return a 
+ * Function that tests whether a given String ends with the endsWith 
+ * character.
+ */
+function createEndsWithFilter(string, endsWith) {
+    // YOUR CODE BELOW HERE //
+    
+
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+
+
+
+/**
+ * Fizz Buzz
+ *
+ *  a. Write a Function called fizzBuzz that prints the numbers from 1 to 100.
+ *  b. BUT, for multiples of three print “Fizz” instead of the number.
+ *  c. AND for the multiples of five print “Buzz”.
+ *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
+*/
+
+function fizzBuzz() {
+    // YOUR CODE GOES BELOW HERE //
+
+
+
+    // YOUR CODE GOES ABOVE HERE //
+}
+
+
+
+
+
+
+/**
+ * reverseString()
+ *
+ *  a. Create a function called reverseString that takes a 
+ *  String as its only input and returns a new String 
+ *  representing the input String reversed.  For example:
+ *  
+ *      reverseString('hello');  // => 'olleh'
+ * 
+ * TIPS:
+ *  a. create something to collect the output you'll return.
+ *  b. you'll need a loop, which one is best? How do you know 
+ *     when to stop looping?
+ *  c. how do you access individual characters of a String?
+ *  d. how do you concatenate Strings? What operator do we use?
+ */
+
+function reverseString(input) {
+    // YOUR CODE GOES BELOW HERE //
+
+
+
+    // YOUR CODE GOES ABOVE HERE //
+}
